228047e29bbaaa94c98c593cb530faba6a3c814d
Pooja Bhagat <66077775+581-pooja@users.noreply.github.com>
2021-06-17 23:35:12 +0530

    Update settings.py

--- textUtilis/settings.py
+++ textUtilis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^o^ijmtxe^wop$0ifgnwaf=udl$w((rloxp_l@brq&42c#qq)s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
